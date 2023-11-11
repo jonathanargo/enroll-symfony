a07277115f0b64533b45ad59bf49ed2c2dd855cd
Jonathan Argo <jonathan.argo@gmail.com>
2023-11-11 12:53:23 -0500

    Added orm package and initial controller

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/student/home', name: 'homepage')]
+    public function home(): Response
+    {
+        return $this->render('student/home.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+}
